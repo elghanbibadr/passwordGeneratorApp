872fe6d702ca75923e7e1cb675505f84ce5dd6bd
badr ghanbi <bghanbi50@gmail.com>
2022-10-23 19:35:42 +0100

    add more functionality

--- src/Javascript/main.js
+++ src/Javascript/main.js
@@ -5,7 +5,9 @@ import {slider} from './slider.js';
 // VARIABELS
 let checkBoxes=document.querySelectorAll('.checkBox');
 let choicesOption={number:0,isLengthGreatherThanEight:false};
-let passwordLength;
+let passwordLength
+let StrengthBoxesColore;
+let passwordContent=document.querySelector('#password')
 let SECURE_LENGTH=8;
 let BoxesPower=document.querySelector('.password-powerBox');
 let PASS_STRENGTH_OPTCION_FOR_LESS_SECURE={1:'too week',2 :'week',3:'week',4:'medium'};
@@ -13,14 +15,23 @@ let PASS_STRENGTH_OPTCION_FOR_MORE_SECURE={1:'too week',2 :'week',3:'medium',4:'
 let checkBoxesArray=Array.from(checkBoxes);
 let passwordBgColor=Array.from(BoxesPower.querySelectorAll(' li'));
 let messge=document.createElement('h1');
+let userChoice=[];
+let passwordOptions={
+  uppercase:'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase:'abcdefghijklmnopqrstuvwxyz',
+  number:'0123456789',
+  symbols:'ŽŠ/.<#&éè-çàç)=$ù!°*€'
+} ;
+let GeneratePasswordBtn=document.querySelector('.generateBtn');
 let mainFooter=document.querySelector('.main__footer');
 // EVENT LISTENER
 
 checkBoxes.forEach(element=>element.addEventListener('click',(e)=>selectChoice(e.target)))
-
+GeneratePasswordBtn.addEventListener('click',GeneratePasswordBtnForUser)
 // FUNCTIONS
 function selectChoice(element){
   element.classList.toggle('colorizeChoiceBox'); 
+  userChoice.push(element);
   //   now i need to update the length of how many choice do i have
   choicesOption.number=checkBoxesArray.filter(v=>v.classList.contains('colorizeChoiceBox')).length;
 //   now we also need to check the label for how we many charectere we have(password length prefered by the user)
@@ -54,7 +65,7 @@ function getPasswordPower(){
 
 // this function responsible for getting the strength of the choisen password options and show it to the user
 function showUserMessage(value='select choice'){
-  userChoice(value);
+  userChoiceStrength(value);
   messge.textContent=`${value}`;
   mainFooter.append(messge);
   messge.classList.add('message');
@@ -63,7 +74,7 @@ function showUserMessage(value='select choice'){
 
 // function to check if user select or not 
 
-function userChoice(value){
+function userChoiceStrength(value){
   if( value=='select choice' ){
     messge.style.color='red';
     setTimeout(()=>messge.style.display='none',2000);
@@ -77,17 +88,73 @@ function userChoice(value){
 
 function colorizeChoiceBoxDependOnPassworedPower(passStrength){
   // lets get the element to be colorized
-  let elementToBeColorized=passwordBgColor.slice(0,choicesOption.number);
+   StrengthBoxesColore=passwordBgColor.slice(0,choicesOption.number);
   
   // lets make bg transparent from all element not to be clorized
    passwordBgColor.forEach(element=>{
-    if (!elementToBeColorized.includes(element))  element.className='passBgPower';
+    if (!StrengthBoxesColore.includes(element))  element.className='passBgPower';
    })
     //  add color for bg depending on password strength
-  elementToBeColorized.forEach((element)=>{
+  StrengthBoxesColore.forEach((element)=>{
      element.className= `passBgPower pass-${passStrength.replace(/\s+/g,'')}`;
   })
 }
 
+// function that takes user prefrencies and generate a random password accordingly
+function GeneratePasswordBtnForUser(){ 
+  passwordLength=slider.label.textContent;
+  if ((!StrengthBoxesColore && passwordLength==0) || (passwordLength==0) ){
+    passwordContent.textContent=''
+    alert('Please make sure that length is at least 1');
+    return;
+  }
+  // we assuming that the user hasn't enetered anything of choice yet
+  if (!StrengthBoxesColore){
+   passwordContent.textContent=generateString(passwordLength);
+   passwordContent.style.color='white'
+
+  }
+  
+  
+  // now lets handle when user select somthing
+   if (StrengthBoxesColore){
+    passwordContent.textContent=generateString(passwordLength,true);
+    passwordContent.style.color='white';
+  
+   }
+ 
+  
+ }
+ 
+
+
+// program to generate random strings
+
+// declare all characters
+
+function generateString(length,userSelectSomething) {
+ 
+  let {lowercase}=passwordOptions;
+  let character=lowercase;
+
+   if (!userSelectSomething){
+      // character+=lowercase;
+      console.log(character)
+   } else{
+      userChoice.forEach(choice=>{
+     let attributeName=choice.getAttribute('data-type');
+     character+=`${passwordOptions[`${attributeName}`]}` ;
+     console.log(character)
+    })
+   }
+   
+    let result = ' ';
+    const charactersLength = character.length;
+    for ( let i = 0; i < +length; i++ ) {
+        result += character.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
 
 

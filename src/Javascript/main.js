e3a76858991cc617ec60c7bcbe975ed2dc9009d6
badr ghanbi <bghanbi50@gmail.com>
2022-10-23 13:45:53 +0100

    rename so variable to more meaningfull names and add function responsible for detecting user choice and add a message accordingly

--- src/Javascript/main.js
+++ src/Javascript/main.js
@@ -6,9 +6,10 @@ import {slider} from './slider.js';
 let checkBoxes=document.querySelectorAll('.checkBox');
 let choicesOption={number:0,isLengthGreatherThanEight:false};
 let passwordLength;
+let SECURE_LENGTH=8;
 let BoxesPower=document.querySelector('.password-powerBox');
-let passwordStrengthOptionsForLengthLessThanEight={1:'too week',2 :'week',3:'week',4:'medium'};
-let passwordStrengthOptionsForLengthMoreThanEight={1:'too week',2 :'week',3:'medium',4:'strong'};
+let PASS_STRENGTH_OPTCION_FOR_LESS_SECURE={1:'too week',2 :'week',3:'week',4:'medium'};
+let PASS_STRENGTH_OPTCION_FOR_MORE_SECURE={1:'too week',2 :'week',3:'medium',4:'strong'};
 let checkBoxesArray=Array.from(checkBoxes);
 let passwordBgColor=Array.from(BoxesPower.querySelectorAll(' li'));
 let messge=document.createElement('h1');
@@ -25,9 +26,8 @@ function selectChoice(element){
 //   now we also need to check the label for how we many charectere we have(password length prefered by the user)
   passwordLength= slider.label.textContent;
   // update the user choices depending on user selection
-  choicesOption.isLengthGreatherThanEight= passwordLength >8 ? true:false;
+  choicesOption.isLengthGreatherThanEight= passwordLength > SECURE_LENGTH ? true:false;
   getPasswordPower();
-
 }
 
 
@@ -35,34 +35,45 @@ function selectChoice(element){
 function getPasswordPower(){
   
   if(!choicesOption.isLengthGreatherThanEight){
-      let strengthForLenghtLessThanEight= passwordStrengthOptionsForLengthLessThanEight[ `${choicesOption.number}`];
-      // let keyForLessThanEight=getKeyByValue(passwordStrengthOptionsForLengthLessThanEight, strengthForLenghtLessThanEight);
+      let STRENGTH_LESS_SECURE= PASS_STRENGTH_OPTCION_FOR_LESS_SECURE[ `${choicesOption.number}`];
       // we need to show the message to user
-      showUserMessage(strengthForLenghtLessThanEight);
+      showUserMessage(STRENGTH_LESS_SECURE);
       // now we need to choose how many boxes should be colorized 
-       colorizeChoiceBoxDependOnPassworedPower(strengthForLenghtLessThanEight);
-      // console.log(keyForLessThanEight)
+       colorizeChoiceBoxDependOnPassworedPower(STRENGTH_LESS_SECURE);
     }
     else {
-      let strengthForLenghtMoreThanEight= passwordStrengthOptionsForLengthMoreThanEight[ `${choicesOption.number}`];
+      let STRENGTH_MORE_SECURE= PASS_STRENGTH_OPTCION_FOR_MORE_SECURE[ `${choicesOption.number}`];
       // let keyForMoreThanEight=getKeyByValue(passwordStrengthOptionsForLengthMoreThanEight, strengthForLenghtMoreThanEight);
-      showUserMessage(strengthForLenghtMoreThanEight);
+      showUserMessage(STRENGTH_MORE_SECURE);
       // now we need to choose how many boxes should be colorized 
-       colorizeChoiceBoxDependOnPassworedPower(strengthForLenghtMoreThanEight)
+       colorizeChoiceBoxDependOnPassworedPower(STRENGTH_MORE_SECURE)
     }
 }
 
 
 
-// this function responsible for getting the strength of the choisen password and show it to the user
+// this function responsible for getting the strength of the choisen password options and show it to the user
 function showUserMessage(value='select choice'){
-  value=='select choice' ? messge.style.color='red':messge.style.color='white';
-
+  userChoice(value);
   messge.textContent=`${value}`;
   mainFooter.append(messge);
   messge.classList.add('message');
   BoxesPower.insertAdjacentElement('beforebegin',messge);
 }
+
+// function to check if user select or not 
+
+function userChoice(value){
+  if( value=='select choice' ){
+    messge.style.color='red';
+    setTimeout(()=>messge.style.display='none',2000);
+   } 
+    else{
+     messge.style.color='white';
+     messge.style.display='block';
+   }
+}
+
 
 function colorizeChoiceBoxDependOnPassworedPower(passStrength){
   // lets get the element to be colorized
@@ -70,7 +81,7 @@ function colorizeChoiceBoxDependOnPassworedPower(passStrength){
   
   // lets make bg transparent from all element not to be clorized
    passwordBgColor.forEach(element=>{
-    if (!elementToBeColorized.includes(element)) element.className='passBgPower';
+    if (!elementToBeColorized.includes(element))  element.className='passBgPower';
    })
     //  add color for bg depending on password strength
   elementToBeColorized.forEach((element)=>{
